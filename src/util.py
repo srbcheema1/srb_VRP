c8e0e48de24ccb0188ad35e3991c3dbe64d2cd23
Sarbjit <srbcheema1@gmail.com>
2020-05-19 23:05:04 +0530

    cleaning

--- src/util.py
+++ src/util.py
@@ -1,5 +1,4 @@
 from .graph import Graph
-from .simulated_annealing import SimulatedAnnealing
 from .kmean import Kmean, Cluster, Point
 from src.const import srb_cap
 
@@ -45,31 +44,3 @@ def sklearn_kmeans(cities):
 		clusters[label[i]].mean_point = Point(k.cluster_centers_[label[i]][0],k.cluster_centers_[label[i]][1],origin)
 
 	return _clusters_to_list(clusters),clusters
-
-'''
-these are useless now, instead use SimulatedAnnealing from .simulated_clusters
-'''
-def combine_histories(clusters_history):
-	combined_history = [] # combined of all clusters
-	for frame_no in range(max([len(history) for history in clusters_history])): # frames as longest history
-		frame = []
-		for cluster_history in clusters_history:
-			if(frame_no < len(cluster_history)):
-				frame.extend(cluster_history[frame_no])
-			else:
-				frame.extend(cluster_history[-1])
-		combined_history.append(frame)
-	return combined_history
-
-
-def anneal_clusters(clusters,cities):
-	# takes k.partition.clusters as param
-	cost = 0
-	clusters_history = [] # ith history represents history of ith cluster
-	for i,cluster in enumerate(clusters):
-		graph = Graph([cities[0]] + [cities[pt.index] for pt in cluster.points])
-		cluster_history,cost_temp = SimulatedAnnealing(graph, graph.nearestNeighbourSolution(), 0.9998 , 10, 0.0000001, 1000000).anneal()
-		cost += cost_temp
-		clusters_history.append([graph.get_indices(path) for path in cluster_history])
-
-	return combine_histories(clusters_history), cost
