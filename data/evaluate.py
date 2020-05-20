f0297575bfe342f95d90ef96524806ef9bd1801f
Sarbjit <srbcheema1@gmail.com>
2020-05-20 21:25:03 +0530

    100 tests

--- data/evaluate.py
+++ data/evaluate.py
@@ -5,20 +5,28 @@ a.load_json('data/out.json')
 
 summ = [0,0,0,0]
 mint = [0,0,0,0]
-for row in a:
-	summ[0] += row[2]
-	summ[1] += row[3]
-	summ[2] += row[4]
-	summ[3] += row[5]
+time = [0,0,0,0]
 
-	minn = min(row[2],row[3],row[4],row[5])
-	if(minn == row[2]): mint[0] += 1
-	if(minn == row[3]): mint[1] += 1
-	if(minn == row[4]): mint[2] += 1
-	if(minn == row[5]): mint[3] += 1
+for row in a:
+	summ[0] += row[3]
+	summ[1] += row[5]
+	summ[2] += row[7]
+	summ[3] += row[9]
+
+	time[0] += row[4]
+	time[1] += row[6]
+	time[2] += row[8]
+	time[3] += row[10]
+
+	minn = min(row[3],row[5],row[7],row[9])
+	if(minn == row[3]): mint[0] += 1
+	elif(minn == row[5]): mint[1] += 1
+	elif(minn == row[7]): mint[2] += 1
+	elif(minn == row[9]): mint[3] += 1
 
 n = len(a) - 1
-avg = [x/n for x in summ]
-print("SA","ACO","ACO-SA","Kmean-SA")
-print(*avg)
-print(*mint)
+avg = [round(x/n,2) for x in summ]
+time = [round(x/n,2) for x in time]
+
+b = Tabular([["Prop","SA","ACO","ACO-SA","Kmean-SA"],["avg cost"] + avg,["avg time"] + time,["min times"] + mint])
+print(b)
--- main.py
+++ main.py
@@ -28,22 +28,29 @@ def run(cities,cap=10,disp=True):
 	skmean_history, skmean_cost = SimulatedAnnealing(graph,kmean_path,*params).anneal()
 	times.append(time.time())
 
-	times = [times[i]-times[i-1] for i in range(1,len(times))]
+	times = [round(times[i]-times[i-1],2) for i in range(1,len(times))]
 	if(disp): print("algo: ","SA","ACO","aco-SA","Kmean","SA")
 	if(disp): print("times: ",*times)
 	if(disp): print(sa_cost,aco_cost,saco_cost,skmean_cost)
 	if(disp): DynamicPlot().show(cities,sa_history,aco_history,saco_history,kmean_clusters,skmean_history,graph)
-	return [sa_cost,aco_cost,saco_cost,skmean_cost]
+	c_ret = [round(x,2) for x in [sa_cost,aco_cost,saco_cost,skmean_cost]]
+	t_ret = [round(x,2) for x in [times[0],times[1],times[1] + times[2],times[3] + times[4]]]
+	h_ret = [sa_history,aco_history,saco_history,skmean_history]
+	return c_ret, t_ret, h_ret
 
 def extract_output():
-	data = [["N","Cap","SA","ACO","ACO-SA","Kmean-SA"]]
+	data = [["Sno","N","Cap","SA","SA-time","ACO","ACO-time","ACO-SA","ACO-SA-time","Kmean-SA","Kmean-SA-time"]]
 	for i in range(100):
 		cities = City.generate_cities()
 		n = len(cities)
 		cap = 10 if n <=60 else 20
-		ret = run(cities,cap,disp=False)
-		data.append([n,cap] + ret)
-		print(Tabular([[i,n,cap] + ret]))
+		c_ret, t_ret, h_ret = run(cities,cap,disp=False)
+		tabular_ret = [i+1,n,cap]
+		for a,b in zip(c_ret,t_ret):
+			tabular_ret.append(a)
+			tabular_ret.append(b)
+		data.append(tabular_ret)
+		print(Tabular([tabular_ret]))
 	tabular = Tabular(data)
 	tabular.write_json('output/out.json')
 	tabular.write_xls('output/data.xls')
